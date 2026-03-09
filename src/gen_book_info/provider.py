12d0ed88d7e724524ec3aed2beb6a3d64c784259
lesguillemets <kgkacko+github@gmail.com>
2026-03-09 20:17:59 +0900

    lic

--- src/gen_book_info/provider.py
+++ src/gen_book_info/provider.py
@@ -10,3 +10,9 @@ class Provider(ABC):
         from an isbn, gets the book data
         """
         ...
+
+
+# Copyright (c) 2026 lesguillemets
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
