85bbea1b968a89ae9657aa47025171afab090a3c
lesguillemets <kgkacko+github@gmail.com>
2026-03-11 10:37:28 +0900

    lisence text

--- src/gen_book_info/providers/cinii.py
+++ src/gen_book_info/providers/cinii.py
@@ -46,3 +46,9 @@ class CiNiiProvider(Provider):
             urls=[the_result["@id"]],
             year=the_result["dc:date"],
         )
+
+
+# Copyright (c) 2026 lesguillemets
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
